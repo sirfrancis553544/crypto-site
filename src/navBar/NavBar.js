8fcb339e4848c79f581a7e5da131109ee1298302
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-12-05 00:35:15 +0800

    responsive menu

--- src/navBar/NavBar.js
+++ src/navBar/NavBar.js
@@ -1,16 +1,29 @@
 import React from "react";
+import ham from "./ham.svg"
+import close from "./ham-close.svg"
+
 
 export const NavBar = () => {
   return (
     <>
-      {" "}
-      <div className="navbar">
-        <span className="navbar welcome">LOGO</span>
-        <ul className="navbar">Home</ul>
-        <ul className="navbar">Stats</ul>
-        <ul className="navbar">News</ul>
-      </div>
-      <div className="line"></div>
+    
+     
+  <div className="hamburger-menu">
+    <input id="menu__toggle" type="checkbox" />
+    <label className="menu__btn" for="menu__toggle">
+      <span></span>
+    </label>
+
+    <ul className="menu__box">
+      <li><a className="menu__item" href="#">Главная</a></li>
+			<li><a className="menu__item" href="#">Проекты</a></li>
+			<li><a className="menu__item" href="#">Команда</a></li>
+			<li><a className="menu__item" href="#">Блог</a></li>
+			<li><a className="menu__item" href="#">Контакты</a></li>
+    </ul>
+  </div>
+  
+
 
 
     </>
