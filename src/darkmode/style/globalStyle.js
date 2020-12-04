a4d278d35994e9fbf6831cab9f64fe97ff561a9e
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-12-05 01:00:13 +0800

    menu

--- src/darkmode/style/globalStyle.js
+++ src/darkmode/style/globalStyle.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-body, a{
+body, a .menu__box{
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
     font-family: "Roboto", sans-serif;
--- src/navBar/NavBar.js
+++ src/navBar/NavBar.js
@@ -1,31 +1,34 @@
 import React from "react";
-import ham from "./ham.svg"
-import close from "./ham-close.svg"
-
+import ham from "./ham.svg";
+import close from "./ham-close.svg";
 
 export const NavBar = () => {
   return (
     <>
-    
-     
-  <div className="hamburger-menu">
-    <input id="menu__toggle" type="checkbox" />
-    <label className="menu__btn" for="menu__toggle">
-      <span></span>
-    </label>
+      <div className="hamburger-menu">
+        <input id="menu__toggle" type="checkbox" />
+        <label className="menu__btn" for="menu__toggle">
+          <span></span>
+        </label>
 
-    <ul className="menu__box">
-      <li><a className="menu__item" href="#">Главная</a></li>
-			<li><a className="menu__item" href="#">Проекты</a></li>
-			<li><a className="menu__item" href="#">Команда</a></li>
-			<li><a className="menu__item" href="#">Блог</a></li>
-			<li><a className="menu__item" href="#">Контакты</a></li>
-    </ul>
-  </div>
-  
-
-
-
+        <ul className="menu__box">
+          <li>
+            <a className="menu__item" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="menu__item" href="#">
+              Stats
+            </a>
+          </li>
+          <li>
+            <a className="menu__item" href="#">
+              News
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
